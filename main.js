4bd360e46e0cbf3287b4055e7dc6bacb9a7e5545
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-10-15 23:49:52 -0300

    até aula 5

--- main.js
+++ main.js
@@ -2,6 +2,35 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'A pair of warm, fuzzy socks'
+        image: './meiaVerde.jpg',
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./meiaVerde.jpg"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./meiaAzul.png"
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        // addToCart: function () {
+        addToCart(){
+            this.cart += 1
+        },
+
+        cleanCart() {
+            this.cart = 0
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
+
 })
